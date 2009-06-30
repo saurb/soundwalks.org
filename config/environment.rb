fd4989077116cf8168f92dd6b35b259394871b83
Brandon Mechtley <bmechtley+github@gmail.com>
2009-06-29 22:16:45 -0700

    plugin config was incorrect

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,12 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|  
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
-  config.gem "andre-geokit", :source => "http://gems.github.com", :lib => "geokit"
+  config.gem "geokit"
   #config.gem "plant-sirens-ruby", :source => "http://gems.github.com", :lib => "sirens-ruby"
   config.gem "mbleigh-acts-as-taggable-on", :source => "http://gems.github.com", :lib => "acts-as-taggable-on"
   config.gem "rubyist-aasm", :source => "http://gems.github.com", :lib => "aasm"
   
-  config.plugins = [:geokit, :google_maps, :all]
+  #config.plugins = [:geokit, :google_maps, :all]
 
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
