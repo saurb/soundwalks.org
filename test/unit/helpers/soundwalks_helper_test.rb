c479e51de91651465928fb8f9c3cc283443d9b4a
Brandon Mechtley <bmechtley+github@gmail.com>
2009-06-29 17:31:32 -0700

    random commit

---
+++ test/unit/helpers/soundwalks_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class SoundwalksHelperTest < ActionView::TestCase
+end
---
+++ test/unit/soundwalk_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class SoundwalkTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
