b102114b477b451fc1f75c48e51fd3cb9489994a
Brandon Mechtley <bmechtley+github@gmail.com>
2009-07-11 16:49:30 -0700

    default routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :soundwalks, :has_many => :sounds  
   map.root :controller => 'soundwalks'
   
+  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id.:format' 
+  
   #map.recalculate 'soundwalks/:soundwalk_id/sounds/:id/recalculate', :controller => 'sounds', :action => 'recalculate'
   #map.recalculate 'sounds/:id/recalculate', :controller => 'sounds', :action => 'recalculate'
 end
