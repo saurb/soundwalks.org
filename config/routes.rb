6243fde21145765a452ce806e0a9145135c09612
Brandon Mechtley <bmechtley+github@gmail.com>
2009-07-11 16:36:09 -0700

    no need for sounds to have its own resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :sounds#, :path_names => { :recalculate => 'recalculate'}
-  
-  map.resources :soundwalks, :has_many => :sounds
-  map.recalculate 'soundwalks/:soundwalk_id/sounds/:id/recalculate', :controller => 'sounds', :action => 'recalculate'
-  map.recalculate 'sounds/:id/recalculate', :controller => 'sounds', :action => 'recalculate'
-  
+  map.resources :soundwalks, :has_many => :sounds  
   map.root :controller => 'soundwalks'
+  
+  map.recalculate 'soundwalks/:soundwalk_id/sounds/:id/recalculate', :controller => 'sounds', :action => 'recalculate'
+  #map.recalculate 'sounds/:id/recalculate', :controller => 'sounds', :action => 'recalculate'
 end
