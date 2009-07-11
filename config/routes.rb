bf0228d8ee3f6e722cb853a8702fa0c6bb48e41b
Brandon Mechtley <bmechtley+github@gmail.com>
2009-07-11 16:38:11 -0700

    test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :soundwalks, :has_many => :sounds  
   map.root :controller => 'soundwalks'
   
-  map.recalculate 'soundwalks/:soundwalk_id/sounds/:id/recalculate', :controller => 'sounds', :action => 'recalculate'
+  #map.recalculate 'soundwalks/:soundwalk_id/sounds/:id/recalculate', :controller => 'sounds', :action => 'recalculate'
   #map.recalculate 'sounds/:id/recalculate', :controller => 'sounds', :action => 'recalculate'
 end
