4cf66593d402b89daeb251d6dc239823c4418f47
Brandon Mechtley <bmechtley+github@gmail.com>
2009-07-13 13:05:54 -0700

    average latitude and longitude

--- app/models/soundwalk.rb
+++ app/models/soundwalk.rb
@@ -11,10 +11,18 @@ class Soundwalk < ActiveRecord::Base
     
     self.locations = Hash.new
     
+    total_lat = 0
+    total_lng = 0
+    
     lines.each do |line|
       components = line.split(',')
             
       self.locations[components[0].to_i] = [components[1].to_f, components[2].to_f]
+      total_lat += components[1].to_f
+      total_lng += components[2].to_f
     end
+    
+    self.lat = total_lat / lines.size
+    self.lng = total_lng / lines.size
   end
 end
