e55834d4e54512aa08a18694a58ef0ef9b8b7946
Brandon Mechtley <bmechtley+github@gmail.com>
2009-08-04 19:48:15 -0700

    deleted plugins etc.

--- config/environment.rb
+++ config/environment.rb
@@ -6,14 +6,12 @@ ENV['GEM_PATH'] = '/home/brandon/.gem/ruby/gems/1.8:/usr/local/lib/ruby/gems/1.8
 
 Rails::Initializer.run do |config|  
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
-  config.time_zone = "Arizona"
   config.gem "avatar"
   config.gem "geokit"
-  config.gem "mbleigh-acts-as-taggable-on", :source => "http://gems.github.com", :lib => "acts-as-taggable-on"
   config.gem "rubyist-aasm", :source => "http://gems.github.com", :lib => "aasm"
   config.gem "mattetti-googlecharts", :source => "http://gems.github.com", :lib => "gchart"
   config.gem "RedCloth"
-  config.plugins = [:geokit, :google_maps, :all]
+  config.plugins = [:geokit, :all]
 
   config.time_zone = 'UTC'
   
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,15 @@ ActionController::Routing::Routes.draw do |map|
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
   map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
-  map.recalculate '/soundwalks/:soundwalk_id/sounds/:id/recalculate', :controller => 'sounds', :action => 'recalculate'  
-  map.uploader '/soundwalks/:soundwalk_id/sounds/uploader', :controller => 'sounds', :action => 'uploader'
+  map.settings '/settings', :controller => 'users', :action => 'settings'
+    
+  map.resources :soundwalks do |soundwalks|
+    soundwalks.resources :sounds, :collection => {:delete_multiple => :delete, :uploader => :get} do |sounds|
+      sounds.set_tags 'tags', :controller => 'tags', :action => 'update', :method => 'put'
+      sounds.tags 'tags', :controller => 'tags', :action => 'show', :method => 'get'
+    end
+  end
   
-  map.resources :soundwalks, :has_many => :sounds
   map.resources :friendships
   map.resources :users, :member => {:suspend => :put, :unsuspend => :put, :purge => :delete}
   
@@ -18,6 +23,5 @@ ActionController::Routing::Routes.draw do |map|
   map.follow '/:username/follow', :controller => 'users', :action => 'follow'
   map.followers '/:username/followers', :controller => 'users', :action => 'followers'
   map.following '/:username/following', :controller => 'users', :action => 'following'
-  
   map.root :controller => 'home', :action => 'index'
 end
