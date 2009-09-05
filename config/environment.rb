fbcbec4fa4377c12fd8a709ec063936ab740bef0
Brandon Mechtley <bmechtley+github@gmail.com>
2009-09-05 01:38:35 -0700

    .

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,9 @@
+
+ENV['GEM_PATH'] = '/Users/brandon/gems:/usr/local/lib/ruby/1.8/gems'
+
 require File.join(File.dirname(__FILE__), 'boot')
 
 RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
-
-ENV['GEM_PATH'] = '/Users/brandon/gems:/usr/local/lib/ruby/1.8/gems'
   
 Rails::Initializer.run do |config|    
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
