b5058587ca241d1e19a9ee707eb25f98876e26a7
Brandon Mechtley <bmechtley+github@gmail.com>
2009-09-05 01:02:10 -0700

    .

--- vendor/plugins/haml/init.rb
+++ vendor/plugins/haml/init.rb
@@ -1,8 +1,8 @@
-begin
-  require File.join(File.dirname(__FILE__), 'lib', 'haml') # From here
-rescue LoadError
-  require 'haml' # From gem
-end
+#begin
+#  require File.join(File.dirname(__FILE__), 'lib', 'haml') # From here
+#rescue LoadError
+#  require 'haml' # From gem
+#end
 
 # Load Haml and Sass
 Haml.init_rails(binding)
