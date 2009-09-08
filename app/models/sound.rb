908ce3f083252ac4702d3c7cd680ae218fb1d3dd
Brandon Mechtley <bmechtley+github@gmail.com>
2009-09-08 10:43:48 -0700

    .

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -41,93 +41,93 @@ class Sound < ActiveRecord::Base
   end
   
   def analyze_sound
-    #begin
-      #sound_file = Sirens::Sound.new
-      #sound_file.frameLength = 0.04
-      #sound_file.hopLength = 0.02
+    begin
+      sound_file = Sirens::Sound.new
+      sound_file.frameLength = 0.04
+      sound_file.hopLength = 0.02
       
-      #sound_file.open File.join('public', public_filename)
+      sound_file.open File.join('public', public_filename)
       
-      #self.sample_rate = sound_file.sampleRate
-      #self.samples = sound_file.samples
-      #self.frame_length = sound_file.frameLength
-      #self.hop_length = sound_file.hopLength
-      #self.frame_size = sound_file.samplesPerFrame
-      #self.hop_size = sound_file.samplesPerHop
-      #self.spectrum_size = sound_file.spectrumSize
-      #self.frames = sound_file.frames
+      self.sample_rate = sound_file.sampleRate
+      self.samples = sound_file.samples
+      self.frame_length = sound_file.frameLength
+      self.hop_length = sound_file.hopLength
+      self.frame_size = sound_file.samplesPerFrame
+      self.hop_size = sound_file.samplesPerHop
+      self.spectrum_size = sound_file.spectrumSize
+      self.frames = sound_file.frames
       
       # Initialize features.
-      #loudness = Sirens::Loudness.new
-      #temporal_sparsity = Sirens::TemporalSparsity.new
-      #spectral_sparsity = Sirens::SpectralSparsity.new
-      #spectral_centroid = Sirens::SpectralCentroid.new
-      #harmonicity = Sirens::Harmonicity.new
-      #transient_index = Sirens::TransientIndex.new
+      loudness = Sirens::Loudness.new
+      temporal_sparsity = Sirens::TemporalSparsity.new
+      spectral_sparsity = Sirens::SpectralSparsity.new
+      spectral_centroid = Sirens::SpectralCentroid.new
+      harmonicity = Sirens::Harmonicity.new
+      transient_index = Sirens::TransientIndex.new
       
-      #loudness.min = -60
-      #loudness.max = 0
-      #temporal_sparsity.min = 0
-      ##temporal_sparsity.max = 0.5247
-      #spectral_sparsity.min = 0
-      #spectral_sparsity.max = 0.6509
-      #spectral_centroid.min = 0.4994
-      #spectral_centroid.max = 25.7848
-      #transient_index.min = 0.0197
-      #transient_index.max = 42.5258
-      #harmonicity.min = 0
-      #harmonicity.max = 0.5
+      loudness.min = -60
+      loudness.max = 0
+      temporal_sparsity.min = 0
+      temporal_sparsity.max = 0.5247
+      spectral_sparsity.min = 0
+      spectral_sparsity.max = 0.6509
+      spectral_centroid.min = 0.4994
+      spectral_centroid.max = 25.7848
+      transient_index.min = 0.0197
+      transient_index.max = 42.5258
+      harmonicity.min = 0
+      harmonicity.max = 0.5
       
-      #[loudness, temporal_sparsity, spectral_sparsity, spectral_centroid, harmonicity, transient_index].each do |feature|
-      #  feature.maxHistorySize = sound_file.frames
-      #end
+      [loudness, temporal_sparsity, spectral_sparsity, spectral_centroid, harmonicity, transient_index].each do |feature|
+        feature.maxHistorySize = sound_file.frames
+      end
       
-      #[spectral_centroid, harmonicity, transient_index].each do |feature|
-      #  feature.sampleRate = sound_file.sampleRate
-      #  feature.spectrumSize = sound_file.spectrumSize
-      #end
+      [spectral_centroid, harmonicity, transient_index].each do |feature|
+        feature.sampleRate = sound_file.sampleRate
+        feature.spectrumSize = sound_file.spectrumSize
+      end
       
-      #feature_set = Sirens::FeatureSet.new
+      feature_set = Sirens::FeatureSet.new
       
-      #[loudness, temporal_sparsity].each do |feature|
-      #  feature_set.addSampleFeature feature
-      #end
+      [loudness, temporal_sparsity].each do |feature|
+        feature_set.addSampleFeature feature
+      end
       
-      #[spectral_sparsity, spectral_centroid, transient_index, harmonicity].each do |feature|
-      #  feature_set.addSpectralFeature feature
-      #end
+      [spectral_sparsity, spectral_centroid, transient_index, harmonicity].each do |feature|
+        feature_set.addSpectralFeature feature
+      end
       
-      #sound_file.featureSet = feature_set
+      sound_file.featureSet = feature_set
       
       # Extract features.
-      #sound_file.extractFeatures
-      #sound_file.close
+      sound_file.extractFeatures
+      sound_file.close
       
       self.save
       
       loudness_trajectory = self.features.build
       loudness_trajectory.feature_type = :loudness
-      #loudness_trajectory.trajectory = loudness.history
+      loudness_trajectory.trajectory = loudness.history
       
       temporal_sparsity_trajectory = self.features.build
       temporal_sparsity_trajectory.feature_type = :temporal_sparsity
-      #temporal_sparsity_trajectory.trajectory = temporal_sparsity.history
+      temporal_sparsity_trajectory.trajectory = temporal_sparsity.history
       
       spectral_sparsity_trajectory = self.features.build
       spectral_sparsity_trajectory.feature_type = :spectral_sparsity
-      #spectral_sparsity_trajectory.trajectory = spectral_sparsity.history
+      spectral_sparsity_trajectory.trajectory = spectral_sparsity.history
       
       spectral_centroid_trajectory = self.features.build    
       spectral_centroid_trajectory.feature_type = :spectral_centroid
-      #spectral_centroid_trajectory.trajectory = spectral_centroid.history
+      spectral_centroid_trajectory.trajectory = spectral_centroid.history
       
       transient_index_trajectory = self.features.build
       transient_index_trajectory.feature_type = :transient_index
-      #transient_index_trajectory.trajectory = transient_index.history
+      transient_index_trajectory.trajectory = transient_index.history
       
       harmonicity_trajectory = self.features.build
       harmonicity_trajectory.feature_type = :harmonicity
-      #harmonicity_trajectory.trajectory = harmonicity.history
+      harmonicity_trajectory.trajectory = harmonicity.history
       
       loudness_trajectory.save
       spectral_sparsity_trajectory.save
@@ -135,9 +135,9 @@ class Sound < ActiveRecord::Base
       temporal_sparsity_trajectory.save
       transient_index_trajectory.save
       harmonicity_trajectory.save
-    #rescue
-    #  return false
-    #end
+    rescue
+      return false
+    end
   end
   
   def formatted_lat
@@ -157,72 +157,72 @@ class Sound < ActiveRecord::Base
   end
   
   def unpack_feature feature_name
-    #if (feature_name == :loudness)
-    #  feature = Sirens::Loudness.new
-    #elsif (feature_name == :temporal_sparsity)
-    #  feature = Sirens::TemporalSparsity.new
-    #elsif (feature_name == :spectral_sparsity)
-    #  feature = Sirens::SpectralSparsity.new
-    #elsif (feature_name == :spectral_centroid)
-    #  feature = Sirens::SpectralCentroid.new
-    #elsif (feature_name == :transient_index)
-    #  feature = Sirens::TransientIndex.new
-    #elsif (feature_name == :harmonicity)
-    #  feature = Sirens::Harmonicity.new
-    #else
-    #  return nil
-    #end
+    if (feature_name == :loudness)
+      feature = Sirens::Loudness.new
+    elsif (feature_name == :temporal_sparsity)
+      feature = Sirens::TemporalSparsity.new
+    elsif (feature_name == :spectral_sparsity)
+      feature = Sirens::SpectralSparsity.new
+    elsif (feature_name == :spectral_centroid)
+      feature = Sirens::SpectralCentroid.new
+    elsif (feature_name == :transient_index)
+      feature = Sirens::TransientIndex.new
+    elsif (feature_name == :harmonicity)
+      feature = Sirens::Harmonicity.new
+    else
+      return nil
+    end
     
-    #trajectory = self.features.find(:all, :conditions => {:feature_type => feature_name}).first.trajectory
+    trajectory = self.features.find(:all, :conditions => {:feature_type => feature_name}).first.trajectory
     
-    #feature.maxHistorySize = trajectory.size
-    #trajectory.each {|value| feature.addHistoryFrame value.to_f}
-    #puts trajectory
-    #return feature
+    feature.maxHistorySize = trajectory.size
+    trajectory.each {|value| feature.addHistoryFrame value.to_f}
+    puts trajectory
+    return feature
   end
   
   def segments
-    #loudness = unpack_feature(:loudness)
-    #spectral_sparsity = unpack_feature(:spectral_sparsity)
-    #spectral_centroid = unpack_feature(:spectral_centroid)
+    loudness = unpack_feature(:loudness)
+    spectral_sparsity = unpack_feature(:spectral_sparsity)
+    spectral_centroid = unpack_feature(:spectral_centroid)
     
-    #feature_set = Sirens::FeatureSet.new     
-    #feature_set.addSampleFeature loudness
-    #feature_set.addSpectralFeature spectral_sparsity
-    #feature_set.addSpectralFeature spectral_centroid
+    feature_set = Sirens::FeatureSet.new     
+    feature_set.addSampleFeature loudness
+    feature_set.addSpectralFeature spectral_sparsity
+    feature_set.addSpectralFeature spectral_centroid
     
-    #loudness.segmentationParameters.alpha = 0.15
-    #loudness.segmentationParameters.r = 0.0098
-    #loudness.segmentationParameters.cStayOff = 0.0015
-    #loudness.segmentationParameters.cTurnOn = 0.085
-    #loudness.segmentationParameters.cTurningOn = 0.085
-    #loudness.segmentationParameters.cTurnOff = 0.085
-    #loudness.segmentationParameters.cNewSegment = 0.085
-    #loudness.segmentationParameters.cStayOn = 0.05
-    #loudness.segmentationParameters.pLagPlus = 0.75
-    #loudness.segmentationParameters.pLagMinus = 0.75
+    loudness.segmentationParameters.alpha = 0.15
+    loudness.segmentationParameters.r = 0.0098
+    loudness.segmentationParameters.cStayOff = 0.0015
+    loudness.segmentationParameters.cTurnOn = 0.085
+    loudness.segmentationParameters.cTurningOn = 0.085
+    loudness.segmentationParameters.cTurnOff = 0.085
+    loudness.segmentationParameters.cNewSegment = 0.085
+    loudness.segmentationParameters.cStayOn = 0.05
+    loudness.segmentationParameters.pLagPlus = 0.75
+    loudness.segmentationParameters.pLagMinus = 0.75
     
-    #spectral_centroid.segmentationParameters.alpha = 0.05
-    #spectral_centroid.segmentationParameters.r = 0.00000196
-    #spectral_centroid.segmentationParameters.cStayOff = 0.0000933506
-    #spectral_centroid.segmentationParameters.cTurnOn = 0.85
-    #spectral_centroid.segmentationParameters.cTurningOn = 0.85
-    #spectral_centroid.segmentationParameters.cTurnOff = 0.85
-    #spectral_centroid.segmentationParameters.cNewSegment = 0.85
-    #spectral_centroid.segmentationParameters.cStayOn = 0.0025296018
-    #spectral_centroid.segmentationParameters.pLagPlus = 0.75
-    #spectral_centroid.segmentationParameters.pLagMinus = 0.075
+    spectral_centroid.segmentationParameters.alpha = 0.05
+    spectral_centroid.segmentationParameters.r = 0.00000196
+    spectral_centroid.segmentationParameters.cStayOff = 0.0000933506
+    spectral_centroid.segmentationParameters.cTurnOn = 0.85
+    spectral_centroid.segmentationParameters.cTurningOn = 0.85
+    spectral_centroid.segmentationParameters.cTurnOff = 0.85
+    spectral_centroid.segmentationParameters.cNewSegment = 0.85
+    spectral_centroid.segmentationParameters.cStayOn = 0.0025296018
+    spectral_centroid.segmentationParameters.pLagPlus = 0.75
+    spectral_centroid.segmentationParameters.pLagMinus = 0.075
     
-    #spectral_sparsity.segmentationParameters.alpha = 0.05
-    #spectral_sparsity.segmentationParameters.r = 0.0196
-    #spectral_sparsity.segmentationParameters.cStayOff = 0.001833506
-    #spectral_sparsity.segmentationParameters.cTurnOn = 0.85
-    #spectral_sparsity.segmentationParameters.cTurningOn = 0.85
-    #spectral_sparsity.segmentationParameters.cTurnOff = 0.85
-    #spectral_sparsity.segmentationParameters.cNewSegment = 0.85
-    #spectral_sparsity.segmentationParameters.cStayOn = 0.009296018
-    #spectral_sparsity.segmentationParameters.pLagPlus = 0.75
-    #spectral_sparsity.segmentationParameters.pLagMinus = 0.75
+    spectral_sparsity.segmentationParameters.alpha = 0.05
+    spectral_sparsity.segmentationParameters.r = 0.0196
+    spectral_sparsity.segmentationParameters.cStayOff = 0.001833506
+    spectral_sparsity.segmentationParameters.cTurnOn = 0.85
+    spectral_sparsity.segmentationParameters.cTurningOn = 0.85
+    spectral_sparsity.segmentationParameters.cTurnOff = 0.85
+    spectral_sparsity.segmentationParameters.cNewSegment = 0.85
+    spectral_sparsity.segmentationParameters.cStayOn = 0.009296018
+    spectral_sparsity.segmentationParameters.pLagPlus = 0.75
+    spectral_sparsity.segmentationParameters.pLagMinus = 0.75
     
     #segmenter = Sirens::Segmenter.new
     #segmenter.featureSet = feature_set
