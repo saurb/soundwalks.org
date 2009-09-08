119d6b71747e242ba1aa171bc0d7ae8005503567
Brandon Mechtley <bmechtley+github@gmail.com>
2009-09-08 15:21:20 -0700

    .

--- app/models/study.rb
+++ app/models/study.rb
@@ -2,7 +2,7 @@ class Study < ActionMailer::Base
   def study(email_params)
     subject "[soundwalks.org] " << email_params[:subject]
     recipients "study@soundwalks.org"
-    from email_pararms[:email]
+    from email_params[:email]
     sent_on Time.now.utc
     
     body :message => email_params[:body], :name => email_params[:name]
