77702259340319c96c4ecaa10e64467359582d65
Brandon Mechtley <bmechtley+github@gmail.com>
2009-09-08 16:29:26 -0700

    .

--- app/models/study.rb
+++ app/models/study.rb
@@ -2,7 +2,8 @@ class Study < ActionMailer::Base
   def study(email_params)
     sound_ids = email_params[:tags].collect {|key, value| value}
     sound_names = email_params[:names].collect {|key, value| value}
-    tags = email_params[:tags].collect {|key, value| value}
+    tags = email_params[:tags].collect {|key, value| value.split(",").collect {|tag| tag.chomp}}
+    
     sounds = Sound.find(:all, :conditions => {:id => sound_ids})
     sounds.each do |sound|
       sound.study_coverage += 1
@@ -13,6 +14,10 @@ class Study < ActionMailer::Base
     recipients "study@soundwalks.org"
     from "study@soundwalks.org"
     sent_on Time.now.utc
+    
+    puts sound_ids
+    puts sound_names
+    puts tags
     
     body :ids => sound_ids, :names => sound_names, :tags => tags
   end
