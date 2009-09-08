0cd4f5ceaeaf71549bf0b3852fcaf22783461658
Brandon Mechtley <bmechtley+github@gmail.com>
2009-09-08 13:19:37 -0700

    .

---
+++ app/controllers/study_controller.rb
@@ -0,0 +1,13 @@
+class StudyController < ApplicationController
+  def index
+  end
+  
+  def create
+    if Study.deliver_contact(params[:contact])
+      flash[:notice] = "Thank you for participating in our study!"
+      redirect_to(contact_path)
+    else
+      flash.now[:error] = "An error occurred while submitting your form. Please try again or conact study@soundwalks.org."
+      render :index
+    end
+end
---
+++ app/models/study.rb
@@ -0,0 +1,10 @@
+class Study < ActionMailer::Base
+  def contact(email_params)
+    subject "[soundwalks.org] " << email_params[:subject]
+    recipients "study@soundwalks.org"
+    from email_pararms[:email]
+    sent_on Time.now.utc
+    
+    body :message => email_params[:body], :name => email_params[:name]
+  end
+end
