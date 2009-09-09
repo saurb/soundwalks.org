cf7577aa4f34aa6fd9a1dfdacc3f30192fdb224a
Brandon Mechtley <bmechtley+github@gmail.com>
2009-09-09 15:43:05 -0700

    .

--- app/controllers/study_controller.rb
+++ app/controllers/study_controller.rb
@@ -1,13 +1,11 @@
 class StudyController < ApplicationController
-  layout 'site'
-  
   def index
     @sounds = Sound.find(:all, :order => 'study_coverage ASC', :limit => 10)
   end
   
   def create
     if Study.deliver_study(params[:ids], params[:tags])
-      flash[:notice] = "Thank you for participating in our study! If you would like to help us out more, just continue tagging sounds. Each time you complete a set of sounds, you will be given a new set."
+      flash[:notice] = "Your responses were successfully recorded. If you would like to help us out more, just continue tagging sounds. Each time you complete a set of sounds, you will be given a new set."
       redirect_to(study_path)
     else
       flash.now[:error] = "An error occurred while submitting your form. Please try again or contact study@soundwalks.org."
