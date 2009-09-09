181180fef93c896a0a3b1b069522792214d6d778
Brandon Mechtley <bmechtley+github@gmail.com>
2009-09-08 22:24:23 -0700

    .

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -144,7 +144,7 @@ class Sound < ActiveRecord::Base
   end
   
   def create_preview
-    system("lame --quiet -b16 #{self.full_filename}")
+    system("lame --quiet -b16 #{self.full_filename} #{self.full_filename}.mp3")
   end
   
   def formatted_lat
