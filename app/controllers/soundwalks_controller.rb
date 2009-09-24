f135db5257acea73b7d9cac5cb5c5507601692bc
Brandon Mechtley <bmechtley+github@gmail.com>
2009-09-24 15:42:40 -0700

    .

--- app/controllers/soundwalks_controller.rb
+++ app/controllers/soundwalks_controller.rb
@@ -28,7 +28,7 @@ class SoundwalksController < ApplicationController
     respond_to do |format|
       format.html
       format.xml {render :xml => @soundwalk}
-      format.js {render :json => @soundwalk.to_json(:methods => [:formatted_description, :formatted_lat, :formatted_lng]), :callback => params[:callback]}
+      format.js {render :json => @soundwalk.to_json(:methods => [:formatted_description, :formatted_lat, :formatted_lng]), :status => :ok, :callback => params[:callback]}
     end
   end
   
