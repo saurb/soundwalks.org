41f762948ba3936e871228cd6fa42897bfff4238
Brandon Mechtley <bmechtley+github@gmail.com>
2009-10-06 11:57:38 -0700

    .

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ ActionController::Routing::Routes.draw do |map|
   map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
   map.settings '/settings', :controller => 'users', :action => 'settings'
   
-  map.resource :admin, :collection => {:poll => :get}
+  map.resource :admin, :member => {:poll => :get}
   
   map.resources :links, :collection => {
     :delete_all => :get,
