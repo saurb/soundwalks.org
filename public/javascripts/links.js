f9e494718cfa1b06dfbf2a34a380181859010f51
Brandon Mechtley <bmechtley+github@gmail.com>
2009-10-06 12:43:37 -0700

    .

--- public/javascripts/links.js
+++ public/javascripts/links.js
@@ -1,12 +1,12 @@
 function update_value(url, span, text) {
-	$.getJSON(url, function(data) {if (data.value) {$(span).html(text)}});
+	$.getJSON(url, function(data) {if (data.settings.value) {$(span).html(text)}});
 }
 
 function update_status() {
-	update_value("/admin/poll.js?setting=links_weights_acoustic", "#acoustic", "Acoustic weights are being computed.");
-	update_value("/admin/poll.js?setting=links_weights_semantic", "#semantic", "Semantic weights are being computed.");
-	update_value("/admin/poll.js?setting=links_weights_social", "#social", "Social weights are being computed.");
-	update_value("/admin/poll.js?setting=links_distances", "#distances", "Shortest-path distances are being computed.");
+	update_value("/admin/poll.js?setting=links_weights_acoustic", "#acoustic", "Acoustic weights are being computed.<br />");
+	update_value("/admin/poll.js?setting=links_weights_semantic", "#semantic", "Semantic weights are being computed.<br />");
+	update_value("/admin/poll.js?setting=links_weights_social", "#social", "Social weights are being computed.<br />");
+	update_value("/admin/poll.js?setting=links_distances", "#distances", "Shortest-path distances are being computed.<br />");
 }
 
 $(document).ready(function() {setInterval('update_status()', 5000)});
