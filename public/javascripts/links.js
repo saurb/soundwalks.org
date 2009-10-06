be6322305276e0d143cad2c3dbce606dea99cc4a
Brandon Mechtley <bmechtley+github@gmail.com>
2009-10-06 12:54:17 -0700

    .

--- public/javascripts/links.js
+++ public/javascripts/links.js
@@ -1,5 +1,5 @@
 function update_value(url, span, text) {
-	$.getJSON(url, function(data) {if (data['value']) {$(span).html(text)} else {$(span).html('')}});
+	$.getJSON(url, function(data) {alert(data['value']); if (data['value']) {$(span).html(text)} else {$(span).html('')}});
 }
 
 function update_status() {
