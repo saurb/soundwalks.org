a29f6d60c39ebebe05bf166e1ffb7063c0979860
Brandon Mechtley <bmechtley+github@gmail.com>
2009-10-07 13:24:49 -0700

    .

--- app/helpers/string_helper.rb
+++ app/helpers/string_helper.rb
@@ -42,7 +42,7 @@ module StringHelper
     results = Link.query_distribution(@sound, {'Tag' => unique_tag_ids})
     
     for i in 0...results.size
-      results[i][:deviation] = (results[i][:value] * results.size.to_f - 1.0)
+      results[i][:deviation] = (results[i][:value] - (1.0 / results.size.to_f)) / (1.0 / results.size.to_f)
     end
     
     return results.collect{|result| "<span style='font-size: #{(1 + result[:deviation]) * 2}em'>#{result[:name]}</span>"}.join(', ')
