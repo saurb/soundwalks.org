21c80b851889ffd562aad3292f13701d625d4f54
Brandon Mechtley <bmechtley+github@gmail.com>
2009-10-14 16:03:03 -0700

    .

--- db/migrate/20091006031742_sounds_self_similarity.rb
+++
@@ -1,9 +0,0 @@
-class SoundsSelfSimilarity < ActiveRecord::Migration
-  def self.up
-    add_column :sounds, :self_similarity, :float, :default => 0
-  end
-
-  def self.down
-    remove_column :sounds, :self_similarity
-  end
-end
