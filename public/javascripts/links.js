bcefbdc76c6a53528dcc3c21a581c935d6c5aa5c
Brandon Mechtley <bmechtley+github@gmail.com>
2009-10-22 01:29:20 -0700

    abstracted things into progress.js

--- public/javascripts/links.js
+++ public/javascripts/links.js
@@ -1,22 +1,8 @@
-function update_value(url, span, text) {
-	if ($(span + ' .progress_text').text() != "100.00%") {
-		$.getJSON(url, function(data) {
-			if (data['value'] != undefined) {
-				$(span + ' .progress').css({'width' : (data['value'] * 100).toFixed(2).toString() + '%'})
-				$(span + ' .progress_text').html((data['value'] * 100).toFixed(2).toString() + '%')
-			} else {
-				$(span + ' .progress').css({'width' : '0'});
-				$(span + ' .progress_text').html("0.00%")
-			}
-		});
-	}
+function update_all() {
+	update_progress("/admin/poll.js?setting=links_weights_acoustic", "#acoustic");
+	update_progress("/admin/poll.js?setting=links_weights_semantic", "#semantic");
+	update_progress("/admin/poll.js?setting=links_weights_social", "#social");
+	update_progress("/admin/poll.js?setting=links_distances", "#distances");
 }
 
-function update_status() {
-	update_value("/admin/poll.js?setting=links_weights_acoustic", "#acoustic");
-	update_value("/admin/poll.js?setting=links_weights_semantic", "#semantic");
-	update_value("/admin/poll.js?setting=links_weights_social", "#social");
-	update_value("/admin/poll.js?setting=links_distances", "#distances");
-}
-
-$(document).ready(function() {setInterval('update_status()', 5000)});
+$(document).ready(function() {setInterval('update_all()', 5000)});
