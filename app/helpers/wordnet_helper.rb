e91c9915c68554e46d19677145d6b42f4e40f2f1
Brandon Mechtley <bmechtley+github@gmail.com>
2009-10-25 14:02:11 -0700

    semantic similarity. jcn works!

--- app/helpers/wordnet_helper.rb
+++ app/helpers/wordnet_helper.rb
@@ -1,34 +1,79 @@
 module WordnetHelper
-#  lexicon = Wordnet::Lexicon.new
-#  
-#  def wordnet_similarity(a, b)
-#    max_similarity = 0
-#    
-#    parts_of_speech = [WordNet::Noun, WordNet::Adjective, WordNet::Verb, WordNet::Adverb]
-#    
-#    parts_of_speech.each do |part_of_speech|
-#      synsets_a = lexicon.lookupSynset(a, part_of_speech)
-#      synsets_b = lexicon.lookupSynset(b, part_of_speech)
-#      
-#      if synsets_a != nil && synsets_a.size > 0 && synsets_b != nil && synsets_b.size > 0
-#        synsets_a.each do |synset_a|
-#          synsets_b.each do |synset_b|
-#            words_a = synset_a.words
-#            words_b = synset_b.words
-#            all_words = (words_a + words_b).uniq
-#            
-#            total = 0
-#            
-#            words_a.collect{|word| total += 1 if words_b.index(word)}
-#            
-#            similarity = total.to_f / all_words.size.to_f
-#            
-#            max_similarity = similarity if (similarity > max_similarity)
-#          end
-#        end
-#      end
-#    end
-#    
-#    return max_similarity
-#  end
+  Infinity = 1 / 0.0
+  
+  # Returns a list of ancestors for a synset, ascending to its unique beginner (e.g. entity)
+  def ancestors synset
+    ancestors = [synset]
+    
+    while ancestors.last and ancestors.last.hypernyms.size > 0
+      ancestors.push ancestors.last.hypernyms.first
+    end
+    
+    ancestors
+  end
+  
+  # Returns a list of common ancestors between two synsets, ascending.
+  def common_ancestors synset1, synset2
+    ancestors1 = ancestors(synset1)
+    ancestors2 = ancestors(synset2)
+    
+    ancestors1.reject{|synset| !ancestors2.index(synset)}
+  end
+  
+  def frequency synset
+    synset_node = WordnetNode.find(:first, :conditions => {:synset_key => synset.key.to_i})
+    return synset_node ? synset_node.frequency : 0
+  end
+  
+  # Computes the probability of encountering the given synset from the frequency table (i.e. p(word | pos(word)).
+  def probability synset
+    frequency(synset) / WordnetNode.sum(:frequency, :conditions => {:pos => synset.pos, :root => true}).to_f
+  end
+  
+  # Gives the wordsense index of a given synset for its first word.
+  def sense synset
+    word = synset.words.first
+    synsets = word.en.synsets
+    
+    index = 0
+    
+    synsets.each_with_index do |synset_sense, i|
+      if synset_sense.key == synset.key
+        index = i
+        break
+      end
+    end
+    
+    index
+  end
+  
+  # Computes the Jiang and Conrath distance between two words.
+  def jcn_distance word1, word2
+    synsets1 = word1.en.synsets
+    synsets2 = word2.en.synsets
+    
+    min_score = Infinity
+    
+    synsets1.each_with_index do |synset1, i|
+      synsets2.each_with_index do |synset2, j|
+        score = Infinity
+        
+        ancestors = common_ancestors(synset1, synset2)
+        
+        if ancestors.size > 0
+          lso = ancestors.first
+          
+          ic1 = -Math.log(probability(synset1))
+          ic2 = -Math.log(probability(synset2))
+          iclso = -Math.log(probability(lso))
+          
+          score = (ic1 + ic2) - (2 * iclso)
+        end
+        
+        min_score = score if min_score > score
+      end
+    end
+    
+    min_score
+  end
 end
