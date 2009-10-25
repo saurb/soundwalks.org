477a60cc6949577d35993943dc3d43aafaac8200
Brandon Mechtley <bmechtley+github@gmail.com>
2009-10-25 13:58:42 -0700

    dont need to cache hypernyms either

--- db/migrate/20091022081438_add_hypernyms_to_tags.rb
+++
@@ -1,9 +0,0 @@
-class AddHypernymsToTags < ActiveRecord::Migration
-  def self.up
-    add_column :tags, :hypernym, :integer
-  end
-
-  def self.down
-    remove_column :tags, :hypernym
-  end
-end
