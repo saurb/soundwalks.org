1691486686449e1cf548f3781cf8abee5a42fd29
Brandon Mechtley <bmechtley+github@gmail.com>
2009-10-27 12:17:39 -0700

    .

--- app/helpers/string_helper.rb
+++ app/helpers/string_helper.rb
@@ -70,15 +70,15 @@ module StringHelper
     html = ''
     
     if style == :normal || style == nil
-      html += results.collect{|result| "<span style='color: rgb(#{result[:r]}, #{result[:g]}, #{result[:b]}); font-size: #{(1.5 + result[:deviation] * 0.25)}em'>#{result[:name]}</span>"}.join(',')
+      html += results.collect{|result| "<span style='color: rgb(#{result[:r]}, #{result[:g]}, #{result[:b]}); font-size: #{(1.5 + result[:deviation] * 0.25)}em'>#{result[:name]}</span>"}.join(', ')
     elsif style == :old
       html_results = []
       results.each do |result|
         color = "#%02x%02x%02x" % [result[:r], result[:g], result[:b]].map {|i| i}
-        html_results.push "<font color='#{color}' size='#{(1.5 + result[:deviation] * 0.25)}em'>#{result[:name]}</font>"
+        html_results.push "<font color='#{color}' size='#{((1.5 + result[:deviation] * 0.25) * 16).to_i}'>#{result[:name]}</font>"
       end
       
-      html = html_results.join(',')
+      html = html_results.join(', ')
     end
     
     html
