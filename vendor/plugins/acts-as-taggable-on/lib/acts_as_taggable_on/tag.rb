ed78227ef60f2a082059d8913605c78559e3a656
Brandon Mechtley <bmechtley+github@gmail.com>
2009-10-30 21:23:27 -0700

    tags now create mds nodes

--- vendor/plugins/acts-as-taggable-on/lib/acts_as_taggable_on/tag.rb
+++ vendor/plugins/acts-as-taggable-on/lib/acts_as_taggable_on/tag.rb
@@ -4,6 +4,10 @@ class Tag < ActiveRecord::Base
   validates_uniqueness_of :name
   has_one :mds_node, :as => :owner, :dependent => :destroy
   
+  after_save do |record|
+    record.create_mds_node
+  end
+  
   # LIKE is used for cross-database case-insensitivity
   def self.find_or_create_with_like_by_name(name)
     find(:first, :conditions => ["name LIKE ?", name]) || create(:name => name)
