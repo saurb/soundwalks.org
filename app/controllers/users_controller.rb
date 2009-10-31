6e9409b7f0a3cddf26c479b5a90f2f7539b4a072
Brandon Mechtley <bmechtley+github@gmail.com>
2009-10-31 13:47:32 -0700

    more user info in data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -79,7 +79,7 @@ class UsersController < ApplicationController
     
     respond_to do |format|
       format.html {render :layout => 'site'}
-      format.xml {render :xml => @user.to_xml(:except => user_exceptions, :methods => user_methods, :include => [:soundwalk_ids, :friend_ids, :inverse_friend_ids])}
+      format.xml {render :xml => @user.to_xml(:except => user_exceptions, :methods => user_methods)}
       format.js {render :json => @user.to_json(:except => user_exceptions, :methods => user_methods)}
     end
   end
@@ -182,6 +182,6 @@ protected
   end
   
   def user_methods
-    [:avatar_large, :avatar_medium, :avatar_small, :avatar_tiny]
+    [:avatar_large, :avatar_medium, :avatar_small, :avatar_tiny, :soundwalk_ids, :friend_ids, :inverse_friend_ids]
   end
 end
