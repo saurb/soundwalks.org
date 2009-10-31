7dd3e25cadc9f995377ea98baa4d354580b0a527
Brandon Mechtley <bmechtley+github@gmail.com>
2009-10-31 13:51:50 -0700

    more user info in data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -182,6 +182,6 @@ protected
   end
   
   def user_methods
-    [:avatar_large, :avatar_medium, :avatar_small, :avatar_tiny, :soundwalk_ids, :friend_ids, :inverse_friend_ids]
+    [:avatar_large, :avatar_medium, :avatar_small, :avatar_tiny]
   end
 end
