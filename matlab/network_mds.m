4153d62a0bd7a1ba570de6a93e67bbc2204ad13c
Brandon Mechtley <bmechtley+github@gmail.com>
2009-11-04 14:47:21 -0700

    load from links file instead of distance matrix csv

--- matlab/network_mds.m
+++ matlab/network_mds.m
@@ -2,11 +2,38 @@ clc;
 clear all;
 close all;
 
-distances = csvread('../data/mds/distances.csv');
-ids = csvread('../data/mds/ids.csv');
+%------------------------------%
+% 1. Load distances and costs. %
+%------------------------------%
+disp('Loading data.');
 
-locations = cmdscale(distances);
+links = csvread('../data/mds/linksfixed.csv');
+ids = unique(links(:,2));
+nodes = length(ids);
 
-for i = 1:length(locations)
-    fprintf('%d,%f,%f\n', ids(i), locations(i, 1), locations(i, 2));
+disp('Creating matrices.');
+
+distances = zeros(nodes, nodes);
+costs = zeros(nodes, nodes);
+
+for i = 1:nodes
+    for j = 1:nodes
+        row = (i - 1) * nodes + j;
+        costs(i, j) = links(row, 3);
+        distances(i, j) = links(row, 4);
+    end
+end
+
+size(distances)
+
+%----------------------------%
+% 2. Output MDS coordinates. %
+%----------------------------%
+
+disp('Performing MDS.');
+
+mds = cmdscale(distances);
+
+for i = 1:nodes
+    fprintf('%d,%f,%f\n', ids(i), mds(i, 1), mds(i, 2));
 end
