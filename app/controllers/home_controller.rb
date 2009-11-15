24e365841c4b124582b7fac3a519c3678241a9d8
Brandon Mechtley <bmechtley+github@gmail.com>
2009-11-14 20:01:13 -0700

    map is now main page if you havent logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,15 +18,9 @@ class HomeController < ApplicationController
         format.xml {render :xml => @soundwalks}
         format.json {render :json => @soundwalks, :callback => params[:callback]}
       end
-    else      
-      @pages =  (Soundwalk.count / 20.0).ceil
-      @page = (defined? params[:page]) ? params[:page].to_i : 0
-      @soundwalks = Soundwalk.find(:all, :limit => 20, :offset => @page * 20, :order => "created_at DESC, title", :conditions => {:privacy => 'public'})
-      
+    else
       respond_to do |format|
-        format.html {render :template => 'soundwalks/index'}
-        format.xml {render :xml => @soundwalks}
-        format.json {render :json => @soundwalks, :callback => params[:callback]}
+        format.html {render :template => 'map/show'}
       end
     end
   end
